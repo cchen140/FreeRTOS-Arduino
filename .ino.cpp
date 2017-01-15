dc214714036be96a173f0d8ae489deb193b6ffb1
Chien-Ying Chen <cchen140@illinois.edu>
2017-01-15 16:41:10 -0600

    Initial commit for FreeRTOS_AVR source and example code.

---
+++ .ino.cpp
@@ -0,0 +1,18 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2017-01-15 16:34:51
+
+#include "Arduino.h"
+#include "Arduino.h"
+#include "FreeRTOS_AVR/src/FreeRTOS_AVR.h"
+static void Thread1(void* arg) ;
+static void Thread2(void* arg) ;
+void setup() ;
+void loop() ;
+
+#include "FreeRTOS_Arduino.ino"
+
+
+#endif
